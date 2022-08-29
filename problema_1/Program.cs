452e33f475cb723131776111a428f32a1f1e6c28
IsabellaMuroni <isa_sk_@hotmail.com>
2022-08-29 19:55:12 -0300

    Pto 2 - problema 1

---
+++ problema_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace problema_1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num;
+
+            try 
+            {
+
+                Console.WriteLine("Ingrese un número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("El cuadrado de " + num + " es: " + Cuadrado (num));
+            }
+            catch (OverflowException) {
+
+                Console.WriteLine("ERROR: Desbordamiento");
+            }
+            catch(FormatException){
+
+                Console.WriteLine("ERRO: Formato no aceptado");
+            }
+            catch(Exception ex){
+
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+
+            int Cuadrado (int numero){
+
+                return numero * numero;
+            }
+        }
+    }
+}
