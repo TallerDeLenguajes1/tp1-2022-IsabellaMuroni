f320e05e2239b0ad9cd6b69f2b52fc059d814cc3
IsabellaMuroni <isa_sk_@hotmail.com>
2022-08-29 20:22:39 -0300

    Pto 2 - problema 2

---
+++ problema_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace problema_2
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int num, num1;
+
+            try {
+
+                Console.WriteLine ("Ingrese dividendo: ");
+                num1 = Convert.ToInt32 (Console.ReadLine());
+                Console.WriteLine ("Ingrese divisor: ");
+                num = Convert.ToInt32 (Console.ReadLine());
+                Console.WriteLine ("El cociente entre es: " + Cociente(num1, num));
+
+            }
+            catch (FormatException) {
+
+                Console.WriteLine ("ERROR: Formato no aceptado");
+
+            }
+            catch (System.DivideByZeroException) {
+
+                Console.WriteLine ("ERROR: División por cero");
+
+            }
+            catch (OverflowException) {
+
+                Console.WriteLine ("ERROR: Desbordamiento");
+
+            }
+            catch (Exception ex) {
+
+                Console.WriteLine ("ERROR: " + ex.Message);
+
+            }
+
+
+
+            int Cociente (int divisor, int dividendo) {
+
+                return dividendo / divisor;
+               
+            }
+        }
+    }
+}
