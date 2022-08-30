b9683ae7c6ead72e4272ee88958449b02a9b91d3
IsabellaMuroni <isa_sk_@hotmail.com>
2022-08-29 21:58:38 -0300

    Pto 2 - problema 3

---
+++ problema_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace problema_3
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            int kilometros, litros;
+
+            try
+            {
+                
+                Console.WriteLine ("Ingrese kilómetros: ");
+                kilometros = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine ("Ingrese litros:" );
+                litros = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine ("Los kilómetros por litro son: " + Cociente (litros, kilometros));
+
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine ("ERROR: Desbordamiento");
+            }
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine ("ERROR: División pr cero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine ("ERROR: " + ex.Message);
+            }
+
+
+
+            int Cociente (int divisor, int dividendo) {
+
+                return dividendo / divisor;
+               
+            }
+        }
+    }
+}
